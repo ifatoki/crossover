37e1f9181d7376576866589c7c1a0d14778859a5
Itunuloluwa Fatoki <itunuworks@gmail.com>
2020-07-14 20:26:03 +0100

    chore: fix the tests and let them all show in results

--- src/test/java/com/company/resourceapi/ProjectRepositoryUnitTest.java
+++ src/test/java/com/company/resourceapi/ProjectRepositoryUnitTest.java
@@ -5,18 +5,15 @@ import com.company.resourceapi.entities.SdlcSystem;
 import com.company.resourceapi.repositories.ProjectRepository;
 import com.company.resourceapi.repositories.SdlcSystemRepository;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class ProjectRepositoryUnitTest {
   
